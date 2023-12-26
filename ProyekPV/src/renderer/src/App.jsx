a7f8d0a94c762f42e1df0a7927a0c38addb8804d
Rinko7 <merryfebyanti@gmail.com>
2023-12-26 23:19:35 +0700

    small update ;D

--- ProyekPV/src/renderer/src/App.jsx
+++ ProyekPV/src/renderer/src/App.jsx
@@ -1,10 +1,43 @@
 import Versions from './components/Versions'
 import icons from './assets/icons.svg'
+import { useState } from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import LoginRegister from './components/pages/login/Login'
+import Layout from './components/Layout'
+import Home from './components/pages/home/home'
+
+const router = createBrowserRouter([
+  {
+    element:<Layout/>,
+    children : [
+      {
+        path:'/',
+        element:<Home/>
+      }
+    ]
+  }
+])
 
 function App() {
-  return (
-    <p>Tino mambu wedokan</p>
-  )
+  const [user, setUser] = useState('')
+  const [listUser, setListUser] = useState([])
+
+  if (!user) {
+    return <LoginRegister/>
+  } else {
+    return (
+      <div>
+        <div>
+        {/*
+        welcome 
+        logout button
+        */}
+        </div>
+        <RouterProvider router ={router} />
+      </div>
+    )
+  }
+  
 }
 
 export default App
