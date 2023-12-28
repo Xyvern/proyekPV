c76ddc55a261d084ec85f43c2610e589bccf4e5e
Rinko7 <merryfebyanti@gmail.com>
2023-12-28 22:24:31 +0700

    Search Navbar

--- ProyekPV/src/renderer/src/components/Layout.jsx
+++ ProyekPV/src/renderer/src/components/Layout.jsx
@@ -1,21 +1,20 @@
 import { Box } from "@mui/material";
 import { NavLink, Outlet } from "react-router-dom";
 import './Layout.css'
+import AnimationIcon from '@mui/icons-material/Animation';
 
 const Layout = () => {
   return (
-    // <Box>
-    //   <nav>
-    //     <li><NavLink to="/">Home</NavLink></li>
-    //   </nav>
-    //   <Outlet/>
-    // </Box>
     <Box className="bg-main text-white">
       <Box className="z-50 backdrop-blur sticky top-0">
         <nav className="w-100 h-10vh flex justify-between lg-:py-5 px-5 py-5">
           <div className="flex items-center flex-1">
             {/* <span><IoFastFoodSharp size={32} className="fill-balck-500"/></span> */}
-            <h3 className="text-2xl font-extrabold text-black px-5">Dapur AB</h3>
+            <h3 className="text-2xl font-extrabold text-white px-5">Dapur AB</h3>
+            <Box>
+              <input type="text" placeholder="Search anything..." className="text-white"/>
+              <button>Search</button>
+            </Box>
           </div>
           <div>
             <ul className="flex gap-8 mr-6 text-[18px]">
@@ -23,6 +22,7 @@ const Layout = () => {
               <li><NavLink to="/filter" className="hover:text-gray-500">Filter</NavLink></li>
               <li><NavLink to="/myfav" className="hover:text-gray-500">My Favourites</NavLink></li>
               <li><NavLink to="/profile" className="hover:text-gray-500">Profile</NavLink></li>
+              
             </ul>
           </div>
         </nav>
