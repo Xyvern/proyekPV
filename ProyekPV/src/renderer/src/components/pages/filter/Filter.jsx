112e520edca30805fbf1aed9730ad6916dbe3960
Rinko7 <merryfebyanti@gmail.com>
2023-12-30 01:42:49 +0700

    Filter dikitt

--- ProyekPV/src/renderer/src/components/pages/filter/Filter.jsx
+++ ProyekPV/src/renderer/src/components/pages/filter/Filter.jsx
@@ -7,24 +7,25 @@ import IconButton from '@mui/joy/IconButton';
 import CloseRounded from '@mui/icons-material/CloseRounded';
 const Filter = () => {
   const [value, setValue] = useState('');
+  // ini nanti di kali 3 sama diganti di setiap select sama fungsi close "X" nya ya sist :) aku mager
   const action = useRef(null);
   return (
     <Box>
-      <Select
-      action={action}
-      value={value}
-      placeholder="Category"
-      onChange={(e, newValue) => setValue(newValue)}
-      {...(value && {
-        // display the button and remove select indicator
-        // when user has selected a value
-        endDecorator: (
-          <IconButton
+      {/* Content Select */}
+      <Box className="flex flex-row gap-4">
+        {/* Select Category */}
+        <Select
+        action={action}
+        value={value}
+        placeholder="Category"
+        onChange={(e, newValue) => setValue(newValue)}
+        {...(value && {
+          endDecorator: (
+            <IconButton
             size="sm"
             variant="plain"
             color="neutral"
             onMouseDown={(event) => {
-              // don't open the popup when clicking on this button
               event.stopPropagation();
             }}
             onClick={() => {
@@ -32,22 +33,90 @@ const Filter = () => {
               setValue(null);
               action.current.focusVisible();
             }}
-          >
-            <CloseRounded />
-          </IconButton>
-        ),
-        indicator: null,
-      })}
+              >
+              <CloseRounded />
+            </IconButton>
+          ),
+          indicator: null,
+        })}
 
-      sx={{ maxWidth: 200, color:'rgb(19, 1, 62)', bgcolor:"whitesmoke" }}>
-      <Option value="Anime">Anime</Option>
-      <Option value="cat">Cat</Option>
-      <Option value="fish">Fish</Option>
-      <Option value="bird">Bird</Option>
-    </Select>
-    
+        sx={{ width:'14vw', color:'rgb(19, 1, 62)', bgcolor:"whitesmoke" }}>
+          <Option value="Anime">Anime</Option>
+          <Option value="cat">Cat</Option>
+          <Option value="fish">Fish</Option>
+          <Option value="bird">Bird</Option>
+        </Select>
+        {/* Select Genre */}
+        <Select
+        action={action}
+        value={value}
+        placeholder="Genre"
+        onChange={(e, newValue) => setValue(newValue)}
+        {...(value && {
+          endDecorator: (
+            <IconButton
+            size="sm"
+            variant="plain"
+              color="neutral"
+              onMouseDown={(event) => {
+                event.stopPropagation();
+              }}
+              onClick={() => {
+                console.log(value);
+                setValue(null);
+                action.current.focusVisible();
+              }}
+              >
+              <CloseRounded />
+            </IconButton>
+          ),
+          indicator: null,
+        })}
+        
+        sx={{ width:'14vw', color:'rgb(19, 1, 62)', bgcolor:"whitesmoke" }}>
+          <Option value="Anime">Anime</Option>
+          <Option value="cat">Cat</Option>
+          <Option value="fish">Fish</Option>
+          <Option value="bird">Bird</Option>
+        </Select>
+        {/* Select Sort by */}
+        <Select
+        action={action}
+        value={value}
+        placeholder="Sort by"
+        onChange={(e, newValue) => setValue(newValue)}
+        {...(value && {
+          endDecorator: (
+            <IconButton
+            size="sm"
+            variant="plain"
+              color="neutral"
+              onMouseDown={(event) => {
+                event.stopPropagation();
+              }}
+              onClick={() => {
+                console.log(value);
+                setValue(null);
+                action.current.focusVisible();
+              }}
+              >
+              <CloseRounded />
+            </IconButton>
+          ),
+          indicator: null,
+        })}
+        
+        sx={{ width:'10vw', color:'rgb(19, 1, 62)', bgcolor:"whitesmoke" }}>
+          <Option value="Anime">Anime</Option>
+          <Option value="cat">Cat</Option>
+          <Option value="fish">Fish</Option>
+          <Option value="bird">Bird</Option>
+        </Select>
+      </Box>
+      {/* Content Chip */}
+      {/* Content List Hasil Filter */}
     </Box>
   );
 }
- 
+
 export default Filter;
